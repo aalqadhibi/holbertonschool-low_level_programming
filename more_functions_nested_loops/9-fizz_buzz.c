4ac06e6910607f3b5b18fcc61fd5c571e85d5ddf
aalqadhibi <12425@holbertonstudents.com>
2025-10-23 17:08:40 +0000

    9-fizz_buzz.c

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+int i;
+for (i = 1; i <= 100; i++)
+{
+if (i % 3 == 0 && i % 5 == 0)
+{
+printf("FizzBuzz ");
+continue;
+}
+else if (i % 3 == 0)
+{
+printf("Fizz ");
+continue;
+}
+else if (i % 5 == 0)
+{
+if (i == 100)
+{
+printf("Buzz");
+}
+else
+{
+printf("Buzz ");
+continue;
+}
+}
+else
+{
+printf("%d ", i);
+}
+}
+putchar('\n');
+return (0);
+}
