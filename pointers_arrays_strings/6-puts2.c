1b0f80451431893ab359f15333cc621c9e24f005
aalqadhibi <12425@holbertonstudents.com>
2025-10-24 19:41:49 +0000

    6-puts2.c

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -10,10 +10,15 @@ void puts2(char *str)
 {
 	int i = 0;
 
+	if (!str) /* تحقق من أن المؤشر ليس NULL */
+		return;
+
 	while (str[i] != '\0')
 	{
 		_putchar(str[i]);
-		i += 2;  /* ننتقل حرفين إلى الأمام */
+		if (str[i + 1] == '\0') /* إذا وصلنا للحرف الأخير نتجنب أي تجاوز */
+			break;
+		i += 2;
 	}
 
 	_putchar('\n');
