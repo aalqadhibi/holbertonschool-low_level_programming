7e5ede7a03df564f087b5b3a7cafa62401ace09f
aalqadhibi <12425@holbertonstudents.com>
2025-10-24 19:43:25 +0000

    6-puts2.c

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -2,6 +2,7 @@
 
 /**
  * puts2 - prints every other character of a string
+ * starting with the first character, followed by a new line
  * @str: pointer to the string
  *
  * Return: void
@@ -10,13 +11,13 @@ void puts2(char *str)
 {
 	int i = 0;
 
-	if (!str) /* تحقق من أن المؤشر ليس NULL */
+	if (!str) /* check if the string pointer is NULL */
 		return;
 
 	while (str[i] != '\0')
 	{
 		_putchar(str[i]);
-		if (str[i + 1] == '\0') /* إذا وصلنا للحرف الأخير نتجنب أي تجاوز */
+		if (str[i + 1] == '\0') /* avoid going past the end */
 			break;
 		i += 2;
 	}
