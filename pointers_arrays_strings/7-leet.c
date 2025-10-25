2b8ffbe80eda74a01675444dc2d1eacad0b01cbd
aalqadhibi <12425@holbertonstudents.com>
2025-10-25 15:11:24 +0000

    main.h

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -4,21 +4,21 @@
  * leet - encodes a string into 1337
  * @str: string to encode
  *
- * Return: pointer to the resulting string
+ * Return: pointer to the modified string
  */
 char *leet(char *str)
 {
-    int i, j;
-    char letters[] = "aAeEoOtTlL";
-    char numbers[] = "4433007711";
+	int i, j;
+	char letters[] = "aAeEoOtTlL";
+	char numbers[] = "4433007711";
 
-    for (i = 0; str[i]; i++)
-        for (j = 0; letters[j]; j++)
-            if (str[i] == letters[j])
-            {
-                str[i] = numbers[j];
-                break;
-            }
+	for (i = 0; str[i]; i++)
+		for (j = 0; letters[j]; j++)
+			if (str[i] == letters[j])
+			{
+				str[i] = numbers[j];
+				break;
+			}
 
-    return (str);
+	return (str);
 }
